ff7ed866c2fa1e5ac9ad0d6c3e8aa5a03454f2b0
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2025-02-05 19:31:46 +0100

    Implement weather service with query functionality

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { WeatherQuery } from '../models/weather-query';
+import { WeatherResponse } from '../models/weather-response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  private apiUrl = 'https://ogcie.iblsoft.com/ria/opmetquery';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  queryWeather(query: WeatherQuery): Observable<WeatherResponse> {
+    return this.http.post<WeatherResponse>(this.apiUrl, query);
+  }
+
+  formatWeatherText(text: string): string {
+    return text.replace(/(BKN|FEW|SCT)(\d{3})/g, (match, prefix, number) => {
+      const color = parseInt(number) <= 30 ? 'blue' : 'red';
+      return `<span style="color: ${color}">${prefix}${number}</span>`;
+    });
+  }
 }
